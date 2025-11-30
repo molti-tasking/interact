215302952b11919fbaf4d13265f913c8dbfba2c7
Anton Wolter <tech@antonwolter.de>
2025-12-01 00:20:48 +0100

    Improve css tooling

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -17,15 +17,14 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="mt-8">
-          <ComponentWrapper
-            title="First component"
-            description="First things first. The component below is the first component that is exposed from our package and loaded into this application."
-          >
-            {/* These components require client components. The overall setup of these projects is not perfect yet and the abstraction layer of design and behaviour needs to be found. */}
-            <HiMum />
-          </ComponentWrapper>
-        </div>
+        <ComponentWrapper
+          title="First component"
+          description="First things first. The component below is the first component that is exposed from our package and loaded into this application."
+          className="mt-8"
+        >
+          {/* These components require client components. The overall setup of these projects is not perfect yet and the abstraction layer of design and behaviour needs to be found. */}
+          <HiMum />
+        </ComponentWrapper>
       </main>
     </div>
   );
--- apps/docs/src/components/ComponentWrapper.tsx
+++ apps/docs/src/components/ComponentWrapper.tsx
@@ -1,18 +1,21 @@
+import { cn } from "@/lib/util/cn";
+import { type ClassValue } from "clsx";
 import { PropsWithChildren } from "react";
 
 type ComponentWrapperProps = PropsWithChildren<{
   title: string;
   description: string;
+  className?: ClassValue;
 }>;
 
 export const ComponentWrapper = ({
   title,
   description,
-
+  className,
   children,
 }: ComponentWrapperProps) => {
   return (
-    <div>
+    <div className={cn(className)}>
       <h3 className="text-xl">{title}</h3>
       <p className="mb-4">{description}</p>
       <div className="bg-slate-100 p-8 rounded-xl">{children}</div>
