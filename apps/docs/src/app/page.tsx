70629a9b52b7af24b75b2edadb4c9f38b9d95627
Anton Wolter <tech@antonwolter.de>
2025-12-01 19:40:49 +0100

    add some thoughts on the landing page

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,23 +1,35 @@
 "use client";
-import { AIForm } from "@/components/AIForm";
-import { ComponentWrapper } from "@/components/ComponentWrapper";
-import { HiMum } from "interact";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center ">
-      <main className="flex w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+      <main className="flex w-full max-w-3xl flex-col gap-8 text-zinc-600">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight  ">
-            Interact
-          </h1>
-          <p className="text-zinc-600">
+          <h1 className="text-3xl font-semibold">Interact</h1>
+          <p>
             This is the start of an open source library aiming to build reusable
             and interactable UI components for gen AI systems. We want to go
             beyond chat.
           </p>
-          <p className="text-zinc-600">
-            Checkout the components on the side menu.
+          <p>Checkout the components on the side menu.</p>
+        </div>
+
+        <div className="flex flex-col gap-6">
+          <h2 className="text-2xl font-semibold">
+            What do we actually want from this repository?
+          </h2>
+          <p>
+            There is a ton of generative AI components out there - most of them
+            are around chats and promise the benefits of agentic workflows.
+          </p>
+          <p>
+            In reality, there are sophisticated systems operating our global
+            world for a long time before we even knew of generative AI.
+            Therefore, we strive to build components that can empower ai
+            agnostic systems. We believe a good system should not depend on
+            generative ai and it should just work even without any ai.
+            Nevertheless, we want to empower system builders to implement
+            thoughtful ai interactions into their systems that are opinionated.
           </p>
         </div>
       </main>
